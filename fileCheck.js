6cb312576e47f59ba70a89f1852150f9fc8f8c13
Christian Tschacher <ctschacher@axway.com>
2020-03-04 15:39:12 +0100

    changed algorithm to find/replace different strings

--- fileCheck.js
+++ fileCheck.js
@@ -16,17 +16,20 @@ function fileCheck(args) {
     const content = fs.readFileSync(file, 'utf8');
     let regex = new RegExp('{ .abc }', 'g');
     let count = (content.match(regex)|| []).length;
-    let alteredText = content.replace(/{ .(abc|test123) }/g, "#!#").replace(/{ .[A-Za-z0-9]+ }/g, "!#!");
+    // let alteredText = content.replace(/{ .(abc|test123) }/g, "#!#").replace(/{ .[A-Za-z0-9]+ }/g, "!#!");
+    let alteredText = content.replace(/{ \.[A-Za-z0-9]+ }/g, "#!#").replace(/{ \.[^ ]+ }/g, '!#!');
 
     if(count == 0) console.log("There's no occurrence of the string '{ .abc }'")
-    else console.log(`This file contains ${count} times the string '{ .abc }', will replace it by '#!#'`);
+    else console.log(`This file contains ${count} times the string '{ .abc }'`);
 
-    try {
-            fs.writeFileSync(file, alteredText, 'utf8');
-            console.log('File updated successfully')
-    } catch(err) {
-            console.error(err);
-    }
+    if(content !== alteredText) {  // if change (replacement) occurred write it to file
+        try {
+                fs.writeFileSync(file + '_altered', alteredText, 'utf8');
+                console.log('Created new file:', file + '_altered');
+        } catch(err) {
+                console.error(err);
+        }
+    } else console.log('Nothing to replace')
     
     const lines = alteredText.split(/\r?\n/);
     let stack = [];
@@ -50,5 +53,3 @@ else {
     console.log(`Error: Found ${result.length} ${result[0][0]} curly brackets that have no counterparts`);
     result.forEach((entry) => console.log(`on line ${entry[1]}, pos: ${entry[2]}`)) 
 }
-
-
